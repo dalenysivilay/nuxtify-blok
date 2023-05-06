9030366396a4dce9665ce958dc0712776b4b8bd1
dalenysivilay <hi@daleny.dev>
2023-05-06 11:35:38 -0500

    Enabled devtools

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,12 @@
 export default defineNuxtConfig({
   runtimeConfig: {
     SHOPIFY_STOREFRONT_ENDPOINT: process.env.SHOPIFY_STOREFRONT_ENDPOINT,
-    SHOPIFY_STOREFRONT_ACCESS_TOKEN:
-      process.env.SHOPIFY_STOREFRONT_ACCESS_TOKEN,
+    SHOPIFY_STOREFRONT_ACCESS_TOKEN: process.env.SHOPIFY_STOREFRONT_ACCESS_TOKEN,
   },
-  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/eslint-module'],
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/eslint-module', '@nuxt/devtools'],
+  devtools: {
+    enabled: process.env.NODE_ENV === 'development',
+  },
   typescript: {
     strict: true,
   },
